9b759c8abb0b9b209bae9cb7f9c998eeee828bac
HALKKAT <168440913+HALKKAT@users.noreply.github.com>
2024-05-13 17:17:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/HALKKAT/DAXXMUSIC/blob/Master/config.py",
+    "https://github.com/HALKKAT/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
