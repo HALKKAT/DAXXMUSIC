d09adf114ef6d78975624216ad97c6af4c1cfde6
HALKKAT <168440913+HALKKAT@users.noreply.github.com>
2024-05-20 14:43:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/animendanimeonly")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mrmikeysupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mrmikeysupport")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/animendanimeonly")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
